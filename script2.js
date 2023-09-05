9510fb6cbbc8a4bd1a6c9caa24b0d553b1ad8417
dkimb0 <jhdavidk@gmail.com>
2023-09-04 22:08:36 -0700

    add game pause upon win, put updateDisplay into displayController module

--- script2.js
+++ script2.js
@@ -1,59 +1,142 @@
-const gameBoard = (() => {
+const Player = () => {
+    'use strict'
+    let player = {};
+
+    const _setName = () => {
+        let name = prompt(`What is your name?`);
+        player.name = name;
+    }
+
+    const initName = () => {
+        _setName();
+        _displayName();
+    }
+
+    const _displayName = () => {
+        document.getElementById(`p1`).textContent += `${player.name}`;   
+    }
+
+    const getName = () => player.name;
+
+
+
+    return {
+        initName: initName,
+        getName: getName
+    };
+};
+
+const GameBoard = (() => {
     'use strict';
 
     let board = [];
     let counter;
+    const player1 = Player();
+    const player2 = Player();
+
 
     const init = () => {
+        //initialize new game/players buttons
+        document.querySelector('.newGame').addEventListener('click', function (){
+            _newGame();
+        })
+        document.querySelector('.newPlayers').addEventListener('click', function (){
+            _newPlayers();
+        })
+
+        _initDivs();
+
+        //set up first game and players
+        _newGame();
+        _newPlayers();
+    }
+
+    const _initDivs = () => {
         for (let i = 0; i < 9; i++){
             document.querySelectorAll('.box')[i].addEventListener('click', function (event){
                 _makeMove(event);
-                _updateBoard();
+                displayController.updateDisplay();
 
             })
         }
     }
 
-    const newGame = () => {
+    
+
+
+    //reset board and counter, update display
+    const _newGame = () => {
         board = new Array(' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
-        counter = 1;
-        _updateBoard();
+        counter = 0;
+        console.log(counter);
+        displayController.updateDisplay();
+        _clearWinMessage();
     }
 
-    const _updateBoard = () => {
-        for (let i = 0; i < 9; i++){
-            document.getElementById(`${i+1}`).textContent = board[i];
-        }
+    const _newPlayers = () => {
+        _clearPlayerMessage();
+        player1.initName();
+        document.getElementById(`p1`).textContent += ' vs ';   
+        player2.initName();
     }
 
+    const _clearPlayerMessage = () => {
+        document.getElementById('p1').textContent = '';
+    }
+
+    const _clearWinMessage = () => {
+        document.getElementById('message').textContent = '';
+    }
+
+
+
+    // const _updateDisplay = () => {
+    //     for (let i = 0; i < 9; i++){
+    //         document.getElementById(`${i+1}`).textContent = board[i];
+    //     }
+    // }
+
+    //checks that selected cell is not already taken
     const _checkMove = (cell) => {
         if (board[cell] === ' '){
             return true;
         }
     }
 
+    //changes board array upon user click of cell, moves to next turn
     const _makeMove = (event) => {
-        if (counter%2 === 1 && _checkMove(event.target.id - 1)){
-            board[event.target.id - 1] = 'x';
-
-        }else if(counter%2 === 0 && _checkMove(event.target.id -1)){
-            board[event.target.id - 1] = 'o';
+        console.log(counter);
+        if (_winCheck()){
+            return;
+        }else{
+            ++counter;
+            if (counter%2 === 1 && _checkMove(event.target.id - 1)){
+                board[event.target.id - 1] = 'x';
+    
+            }else if(counter%2 === 0 && _checkMove(event.target.id - 1)){
+                board[event.target.id - 1] = 'o';
+            }
+    
+            _winCheck();
+            
         }
-
-        _winCheck();
-        counter++;
+        
     }
 
+    //if no open moves and no win condition, then its a tie
     const _winCheck = () => {
         if (_checkRow() || _checkCol() || _checkDiag()){
             if (counter%2 === 1){
-                console.log('x won');
+                document.getElementById('message').textContent = player1.getName() + ' wins';
             }else{
-                console.log('o won');
+                document.getElementById('message').textContent = player2.getName() + ' wins';
             }
+            return true;
         }else if (_checkOpenMoves() === 0){
-            console.log('tie');
+            document.getElementById('message').textContent = 'tie';
+            return true;
         }
+        return false;
     }
 
     const _checkRow = () => {
@@ -96,13 +179,30 @@ const gameBoard = (() => {
         return board.filter(value => value.includes(' ')).length;
     }
 
+    const getBoard = () => board;
 
     return {
         init: init,
-        newGame: newGame
+        getBoard: getBoard
     }
 })();
 
+
+const displayController = (() => {
+    'use strict';
+    //update display to state of board
+    console.log(GameBoard.getBoard());
+    const updateDisplay = () => {
+        for (let i = 0; i < 9; i++){
+            document.getElementById(`${i+1}`).textContent = GameBoard.getBoard()[i];
+        }
+    }
+
+    return {
+        updateDisplay: updateDisplay
+    }
+
+})();
+
 //initialize array, update display
-gameBoard.init();
-gameBoard.newGame();
+GameBoard.init();
